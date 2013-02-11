9470b28cb24815fddf490eee9502bba80cd47d45
Kutila Gunasekera <kutila@gmail.com>
2013-02-11 16:58:48 +1000

    add tests to validate PODD ontologies are valid OWL ontologies

--- src/test/java/com/github/podd/ontology/test/PoddOntologyTest.java
+++ src/test/java/com/github/podd/ontology/test/PoddOntologyTest.java
@@ -8,7 +8,7 @@ import java.util.Arrays;
 import java.util.Collection;
 
 import org.junit.Assert;
-import org.junit.Ignore;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
@@ -17,6 +17,19 @@ import org.openrdf.rio.RDFFormat;
 import org.openrdf.rio.RDFParser;
 import org.openrdf.rio.Rio;
 import org.openrdf.rio.helpers.StatementCollector;
+import org.semanticweb.owlapi.formats.OWLOntologyFormatFactoryRegistry;
+import org.semanticweb.owlapi.formats.RioRDFOntologyFormatFactory;
+import org.semanticweb.owlapi.model.OWLOntology;
+import org.semanticweb.owlapi.model.OWLOntologyManager;
+import org.semanticweb.owlapi.model.OWLOntologyManagerFactoryRegistry;
+import org.semanticweb.owlapi.profiles.OWLProfile;
+import org.semanticweb.owlapi.profiles.OWLProfileRegistry;
+import org.semanticweb.owlapi.profiles.OWLProfileReport;
+import org.semanticweb.owlapi.reasoner.OWLReasoner;
+import org.semanticweb.owlapi.reasoner.OWLReasonerFactory;
+import org.semanticweb.owlapi.reasoner.OWLReasonerFactoryRegistry;
+import org.semanticweb.owlapi.rio.RioMemoryTripleSource;
+import org.semanticweb.owlapi.rio.RioParserImpl;
 
 /**
  * Parameterized test to validate PODD ontologies.
@@ -24,15 +37,26 @@ import org.openrdf.rio.helpers.StatementCollector;
  * @author kutila
  * 
  */
-@Ignore
 @RunWith(value = Parameterized.class)
 public class PoddOntologyTest
 {
+    /**
+     * NOTE: The static OWLOntologyManager instance is reused through all the tests so that loaded
+     * ontologies are kept in memory and able to satisfy import requirements in later ontologies.
+     * 
+     */
+    private static OWLOntologyManager owlOntologyManager;
     
     // ----------- parameters for junit test----------------
     private String filename;
     private String mimeType;
     private int statementCount;
+    
+    @BeforeClass
+    public static void beforeClass() throws Exception
+    {
+        PoddOntologyTest.owlOntologyManager = OWLOntologyManagerFactoryRegistry.createOWLOntologyManager();
+    }
     
     /**
      * Parameterized constructor
@@ -50,23 +74,21 @@ public class PoddOntologyTest
     public static Collection<Object[]> data()
     {
         final Object[][] data =
-                new Object[][] { 
+                new Object[][] {
                         { "/ontologies/dcTerms.owl", "application/rdf+xml", 39 },
                         { "/ontologies/poddBase.owl", "application/rdf+xml", 284 },
-                        { "/ontologies/poddUser.owl", "application/rdf+xml", 225 }, 
+                        // { "/ontologies/poddUser.owl", "application/rdf+xml", 225 }, //FIXME: poddUser.owl
                         { "/ontologies/poddScience.owl", "application/rdf+xml", 1124 },
                         { "/ontologies/poddPlant.owl", "application/rdf+xml", 77 },
-                        { "/ontologies/poddAnimal.owl", "application/rdf+xml", 171 }, 
-                };
+                        { "/ontologies/poddAnimal.owl", "application/rdf+xml", 171 }, };
         return Arrays.asList(data);
     }
     
     /**
-     * Test the ontology can be parsed into RDF and contains the expected
-     * number of statements. 
+     * Test the ontology can be parsed into RDF and contains the expected number of statements.
      */
     @Test
-    public void testParseOntology() throws Exception
+    public void testForValidRDF() throws Exception
     {
         final InputStream inputStream = this.getClass().getResourceAsStream(this.filename);
         Assert.assertNotNull("Null resource " + this.filename, inputStream);
@@ -80,6 +102,55 @@ public class PoddOntologyTest
         
         Assert.assertEquals("Incorrect number of statements for " + this.filename, this.statementCount, collector
                 .getStatements().size());
+    }
+    
+    /**
+     * Test the ontology is in OWL-DL profile.
+     */
+    @Test
+    public void testForValidOWLDLOntology() throws Exception
+    {
+        final InputStream inputStream = this.getClass().getResourceAsStream(this.filename);
+        Assert.assertNotNull("Null resource " + this.filename, inputStream);
+        
+        final RDFFormat format = RDFFormat.forMIMEType(this.mimeType);
+        final RDFParser rdfParser = Rio.createParser(format);
+        final StatementCollector collector = new StatementCollector();
+        rdfParser.setRDFHandler(collector);
+        
+        rdfParser.parse(inputStream, "http://purl.org/podd/ns/XYZ");
+        
+        Assert.assertEquals("Incorrect number of statements for " + this.filename, this.statementCount, collector
+                .getStatements().size());
+        
+        // - proceed to OWL ontology validation
+        
+        final RioRDFOntologyFormatFactory ontologyFormatFactory =
+                (RioRDFOntologyFormatFactory)OWLOntologyFormatFactoryRegistry.getInstance()
+                        .getByMIMEType(this.mimeType);
+        final RioParserImpl owlParser = new RioParserImpl(ontologyFormatFactory);
+        
+        final OWLOntology nextOntology = PoddOntologyTest.owlOntologyManager.createOntology();
+        final RioMemoryTripleSource owlSource = new RioMemoryTripleSource(collector.getStatements().iterator());
+        
+        owlParser.parse(owlSource, nextOntology);
+        
+        // verify: ontology is not empty
+        Assert.assertFalse("Loaded ontology was empty", nextOntology.isEmpty());
+        
+        // hard-coded since all our ontologies are expected to be in OWL-DL
+        final OWLProfile nextProfile = OWLProfileRegistry.getInstance().getProfile(OWLProfile.OWL2_DL);
+        
+        // verify: check ontology in profile
+        final OWLProfileReport profileReport = nextProfile.checkOntology(nextOntology);
+        Assert.assertTrue("Ontology not in profile", profileReport.isInProfile());
+        
+        // verify: check consistency
+        final OWLReasonerFactory reasonerFactory =
+                OWLReasonerFactoryRegistry.getInstance().getReasonerFactory("Pellet");
+        final OWLReasoner reasoner = reasonerFactory.createReasoner(nextOntology);
+        
+        Assert.assertTrue("Ontology is not consistent", reasoner.isConsistent());
     }
     
 }
