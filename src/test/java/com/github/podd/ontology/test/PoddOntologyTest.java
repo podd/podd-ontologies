5db2ec7a87048b202ee1f481487d1808cd73101a
Kutila Gunasekera <kutila@gmail.com>
2013-03-07 14:17:24 +1000

    add doNotDisplay annotation to dcterms and foaf concepts

--- src/test/java/com/github/podd/ontology/test/PoddOntologyTest.java
+++ src/test/java/com/github/podd/ontology/test/PoddOntologyTest.java
@@ -80,7 +80,7 @@ public class PoddOntologyTest
                         { "/ontologies/dcTerms.owl", "application/rdf+xml", 39 },
                         { "/ontologies/foaf.owl", "application/rdf+xml", 38 },
                         { "/ontologies/poddUser.owl", "application/rdf+xml", 188 },
-                        { "/ontologies/poddBase.owl", "application/rdf+xml", 269 },
+                        { "/ontologies/poddBase.owl", "application/rdf+xml", 273 },
                         { "/ontologies/poddScience.owl", "application/rdf+xml", 1226 },
                         { "/ontologies/poddPlant.owl", "application/rdf+xml", 77 },
                         { "/ontologies/poddAnimal.owl", "application/rdf+xml", 173 },
