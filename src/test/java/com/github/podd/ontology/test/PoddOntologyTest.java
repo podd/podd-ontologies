af1515753350e8b0f69b6a11cd880b87f529bbe1
Kutila Gunasekera <kutila@gmail.com>
2013-03-20 17:04:55 +1000

    add annotation property poddBase:hasDisplayType and range of values partial adding of annotation to other concepts

--- src/test/java/com/github/podd/ontology/test/PoddOntologyTest.java
+++ src/test/java/com/github/podd/ontology/test/PoddOntologyTest.java
@@ -36,9 +36,7 @@ import org.semanticweb.owlapi.reasoner.OWLReasonerFactory;
 import org.semanticweb.owlapi.reasoner.OWLReasonerFactoryRegistry;
 import org.semanticweb.owlapi.rio.RioMemoryTripleSource;
 import org.semanticweb.owlapi.rio.RioParserImpl;
-import org.semanticweb.owlapi.util.ProgressMonitor;
 
-import com.clarkparsia.owlapi.explanation.BlackBoxExplanation;
 import com.clarkparsia.owlapi.explanation.DefaultExplanationGenerator;
 import com.clarkparsia.owlapi.explanation.ExplanationGenerator;
 import com.clarkparsia.owlapi.explanation.io.ExplanationRenderer;
@@ -93,8 +91,8 @@ public class PoddOntologyTest
                         { "/ontologies/dcTerms.owl", "application/rdf+xml", 39 },
                         { "/ontologies/foaf.owl", "application/rdf+xml", 38 },
                         { "/ontologies/poddUser.owl", "application/rdf+xml", 188 },
-                        { "/ontologies/poddBase.owl", "application/rdf+xml", 273 },
-                        { "/ontologies/poddScience.owl", "application/rdf+xml", 1226 },
+                        { "/ontologies/poddBase.owl", "application/rdf+xml", 314 },
+                        { "/ontologies/poddScience.owl", "application/rdf+xml", 1227 },
                         { "/ontologies/poddPlant.owl", "application/rdf+xml", 77 },
                         { "/ontologies/poddAnimal.owl", "application/rdf+xml", 173 },
                 
