5e4767335ea8d01ab739348ed051656b8ab962bf
Kutila Gunasekera <kutila@gmail.com>
2013-05-13 14:17:24 +1000

    update containedBy restrictions on PoddTopObject and PoddInternalObject

--- src/test/java/com/github/podd/ontology/test/PoddOntologyTest.java
+++ src/test/java/com/github/podd/ontology/test/PoddOntologyTest.java
@@ -92,7 +92,7 @@ public class PoddOntologyTest
                         { "/ontologies/dcTerms.owl", "application/rdf+xml", 39 },
                         { "/ontologies/foaf.owl", "application/rdf+xml", 38 },
                         { "/ontologies/poddUser.owl", "application/rdf+xml", 188 },
-                        { "/ontologies/poddBase.owl", "application/rdf+xml", 315 },
+                        { "/ontologies/poddBase.owl", "application/rdf+xml", 320 },
                         { "/ontologies/poddScience.owl", "application/rdf+xml", 1228 },
                         { "/ontologies/poddPlant.owl", "application/rdf+xml", 233 },
                         { "/ontologies/poddAnimal.owl", "application/rdf+xml", 173 },
