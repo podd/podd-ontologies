40c3424ee935b7982afe6d8e0023d4f2a7dc6567
Peter Ansell <p_ansell@yahoo.com>
2013-07-16 15:21:56 +1000

    Update test numbers

--- src/test/java/com/github/podd/ontology/test/PoddOntologyTest.java
+++ src/test/java/com/github/podd/ontology/test/PoddOntologyTest.java
@@ -75,7 +75,7 @@ public class PoddOntologyTest
                         { "/ontologies/dcTerms.owl", "application/rdf+xml", 39 },
                         { "/ontologies/foaf.owl", "application/rdf+xml", 38 },
                         { "/ontologies/poddUser.owl", "application/rdf+xml", 188 },
-                        { "/ontologies/poddBase.owl", "application/rdf+xml", 362 },
+                        { "/ontologies/poddBase.owl", "application/rdf+xml", 366 },
                         { "/ontologies/poddScience.owl", "application/rdf+xml", 1277 },
                         { "/ontologies/poddPlant.owl", "application/rdf+xml", 237 },
                         { "/ontologies/poddAnimal.owl", "application/rdf+xml", 173 },
