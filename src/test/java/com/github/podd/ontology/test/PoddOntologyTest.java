e9ceb91f5964ee0e4ba2b1916119cfd06d6f7036
Peter Ansell <p_ansell@yahoo.com>
2013-09-17 16:36:42 +1000

    fix test numbers

--- src/test/java/com/github/podd/ontology/test/PoddOntologyTest.java
+++ src/test/java/com/github/podd/ontology/test/PoddOntologyTest.java
@@ -77,7 +77,7 @@ public class PoddOntologyTest
                         { VERSION_1_PATH + "foaf.owl", "application/rdf+xml", 38 },
                         { VERSION_1_PATH + "poddUser.owl", "application/rdf+xml", 188 },
                         { VERSION_1_PATH + "poddBase.owl", "application/rdf+xml", 343 },
-                        { VERSION_1_PATH + "poddScience.owl", "application/rdf+xml", 1303 },
+                        { VERSION_1_PATH + "poddScience.owl", "application/rdf+xml", 1298 },
                         { VERSION_1_PATH + "poddPlant.owl", "application/rdf+xml", 214 },
                         { VERSION_1_PATH + "poddAnimal.owl", "application/rdf+xml", 139 },
                         { VERSION_1_PATH + "poddDataRepository.owl", "application/rdf+xml", 47 },
