b0d19bc5b4e8f92ac934f28867893b9fde4b8dd2
Peter Ansell <p_ansell@yahoo.com>
2014-05-01 16:19:01 +1000

    Use a 1.2-snapshot version of sesame-vocab-builder until it is fixed
    Actual version that is currently relied on is:
    
    d292dc64ad40d68e76068030b909d938cd6c3b6f

---
+++ src/test/java/com/github/podd/ontology/test/PoddVocabBuilderTest.java
@@ -0,0 +1,43 @@
+/**
+ * 
+ */
+package com.github.podd.ontology.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.github.podd.ontologies.PODDBASE;
+
+/**
+ * @author Peter Ansell p_ansell@yahoo.com
+ *
+ */
+public class PoddVocabBuilderTest
+{
+    
+    /**
+     * @throws java.lang.Exception
+     */
+    @Before
+    public void setUp() throws Exception
+    {
+        PODDBASE.ARTIFACT_HAS_TOP_OBJECT.toString();
+    }
+    
+    /**
+     * @throws java.lang.Exception
+     */
+    @After
+    public void tearDown() throws Exception
+    {
+    }
+    
+    @Test
+    public final void test()
+    {
+    }
+    
+}
